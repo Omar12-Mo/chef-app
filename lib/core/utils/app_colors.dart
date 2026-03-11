f3956d283b0aabf1c0d3a71a43b3ba824a01c9fa
omar8157 <mrm548500@gmail.com>
2026-03-12 01:51:02 +0200

    done app_colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xffFA9A0C);
+  static const Color black = Color(0xff1E1E1E);
+  static const Color darkBlack = Color(0xff111111);
+  static const Color textBlack = Color(0xff000000);
+  static const Color white = Colors.white;
+  static const Color grey = Color(0xff9D9FA0);
+  static const Color softgrey = Color(0xff7D8FAB);
+  static const Color red = Color(0xffFB5607);
+
+}
