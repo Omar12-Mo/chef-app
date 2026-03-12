c971b1646bbab54c9a0156f744a21a57dc313cde
omar8157 <mrm548500@gmail.com>
2026-03-12 02:09:05 +0200

    done assets for images

---
+++ lib/generated/assets.dart
@@ -0,0 +1,12 @@
+/// Auto generated file. Do not modify
+class Assets {
+  Assets._();
+
+  static const String imagesBg1 = 'assets/images/bg1.png';
+  static const String imagesBg2 = 'assets/images/bg2.png';
+  static const String imagesChefLoge = 'assets/images/chef_loge.svg';
+  static const String imagesDefaultAvater = 'assets/images/default_avater.png';
+  static const String imagesDeleteMeal = 'assets/images/delete_meal.png';
+  static const String imagesLock = 'assets/images/lock.png';
+  static const String imagesSplash = 'assets/images/Splash.png';
+}
