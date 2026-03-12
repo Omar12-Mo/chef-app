55b11e0eec07a95d3131d84ba27b3bf202331725
omar8157 <mrm548500@gmail.com>
2026-03-12 23:31:27 +0200

    app_text_style Done ✅

---
+++ lib/core/utils/app_text_style.dart
@@ -0,0 +1,149 @@
+import 'package:chef_app/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  static const TextStyle latoBoldBlack36 = TextStyle(
+    fontFamily: "Lato",
+    fontWeight: FontWeight.w700,
+    fontSize: 36,
+    color: AppColors.black,
+  );
+
+
+  static const TextStyle latoRegularBlack24 = TextStyle(
+    fontFamily: "Lato",
+    fontWeight: FontWeight.w400,
+    fontSize: 24,
+    color: AppColors.black,
+  );
+
+
+  static const TextStyle latoBoldBlack24 = TextStyle(
+    fontFamily: "Lato",
+    fontWeight: FontWeight.w700,
+    fontSize: 24,
+    color: AppColors.black,
+  );
+
+
+  static const TextStyle latoRegularBlack16 = TextStyle(
+    fontFamily: "Lato",
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    color: AppColors.black,
+  );
+
+
+  static const TextStyle latoBoldBGrey16 = TextStyle(
+    fontFamily: "Lato",
+    fontWeight: FontWeight.w700,
+    fontSize: 16,
+    color: AppColors.grey,
+  );
+
+
+  static const TextStyle poppinsBoldWhite24 = TextStyle(
+    fontFamily: "Poppins",
+    fontWeight: FontWeight.w700,
+    fontSize: 24,
+    color: AppColors.white,
+  );
+
+
+  static const TextStyle latoBoldWhite32 = TextStyle(
+    fontFamily: "Lato",
+    fontWeight: FontWeight.w700,
+    fontSize: 32,
+    color: AppColors.white,
+  );
+
+
+  static const TextStyle latoBoldWhite14 = TextStyle(
+    fontFamily: "Lato",
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+    color: AppColors.white,
+  );
+
+
+  static const TextStyle latoBoldBlack14 = TextStyle(
+    fontFamily: "Lato",
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+    color: AppColors.black,
+  );
+
+
+  static const TextStyle latoBoldBlack17 = TextStyle(
+    fontFamily: "Lato",
+    fontWeight: FontWeight.w700,
+    fontSize: 17,
+    color: AppColors.black,
+  );
+
+
+  static const TextStyle latoMediumOrange17 = TextStyle(
+    fontFamily: "Lato",
+    fontWeight: FontWeight.w500,
+    fontSize: 17,
+    color: AppColors.primaryColor,
+  );
+ 
+
+  static const TextStyle poppinsRegularOrange16 = TextStyle(
+    fontFamily: "Poppins",
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    color: AppColors.primaryColor,
+  );
+
+
+  static const TextStyle poppinsRegularOrange12 = TextStyle(
+    fontFamily: "Poppins",
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+    color: AppColors.primaryColor,
+  );
+
+
+  static const TextStyle poppinsBoldOrange12 = TextStyle(
+    fontFamily: "Poppins",
+    fontWeight: FontWeight.w700,
+    fontSize: 12,
+    color: AppColors.primaryColor,
+  );
+
+
+  static const TextStyle poppinsRegylarGrey16 = TextStyle(
+    fontFamily: "Poppins",
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    color: AppColors.softgrey,
+  );
+
+
+  static const TextStyle poppinsRegylarBlack16 = TextStyle(
+    fontFamily: "Poppins",
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    color: AppColors.black,
+  );
+
+
+  static const TextStyle poppins600Softgrey24 = TextStyle(
+    fontFamily: "Poppins",
+    fontWeight: FontWeight.w600,
+    fontSize: 24,
+    color: AppColors.softgrey,
+  );
+
+
+  static const TextStyle poppinsBoldBlack16 = TextStyle(
+    fontFamily: "Poppins",
+    fontWeight: FontWeight.w700,
+    fontSize: 16,
+    color: AppColors.black,
+  );
+
+  
+}
