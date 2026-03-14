f93a4fdb4180e0085e4ab427c9e039bce0144718
omar8157 <mrm548500@gmail.com>
2026-03-14 03:46:40 +0200

    name routes done✅

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,3 +1,24 @@
-class Routes{}
+class Routes {
+  static const String initlRoute = "/";
+
+  //auth screens
+  static const String splachScreen = "splachScreen";
+  static const String changeLanguageScreen = "changeLanguageScreen";
+  static const String loginScreen = "loginScreen";
+  static const String forgetPasswordScreen = "forgetPasswordScreen";
+  static const String resetPasswordScreen = "resetPasswordScreen";
+
+
+//profile screens
+  static const String changePasswordScreen = "changePasswordScreen";
+  static const String profileInfoScreen = "profileInfoScreen";
+  static const String updateProfileScreen = "updateProfileScreen";
+
+
+//menu screens
+  static const String addMealScreen = "addMealScreen";
+  static const String menuScreen = "menuScreen";
+ 
+}
 
 class AppRoutes {}
